9bcc799620a39dda7129148b890951a2e36e63f5
Momohjimoh Hashidu <166251551+mjhashid@users.noreply.github.com>
2025-02-09 12:19:01 +0100

    updating the average

--- grade.js
+++ grade.js
@@ -5,9 +5,11 @@ console.log(calculateGrade(scores));
 function calculateGrade(marks) {
   // calculating the average...
 
-  let sum = 0;
-  for (let mark of marks) sum += mark;
-  let average = sum / marks.length;
+  //   let sum = 0;
+  //   for (let mark of marks) sum += mark;
+  //   let average = sum / marks.length;
+
+  let average = calculateAverage(marks);
 
   // Checking the grades...
   if (average < 40) return "F";
@@ -16,4 +18,12 @@ function calculateGrade(marks) {
   if (average < 60) return "C";
   if (average < 70) return "B";
   return "A";
+}
+
+// calculating average using function..
+
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of array) sum += value;
+  return sum / array.length;
 }
